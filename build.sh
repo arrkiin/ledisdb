5a3218c51264b50d5e1221d21fc68f95cc2e62fb
arrkiin <arrkiin@gmail.com>
2019-05-19 12:45:36 +0200

    Multi-Arch-Docker-Build

---
+++ build.sh
@@ -0,0 +1,29 @@
+repo=arrkiin
+project=ledisdb
+
+for docker_arch in amd64 arm32v6 arm64v8; do
+  case ${docker_arch} in
+    amd64   ) build_arch="amd64" ;;
+    arm32v6 ) build_arch="arm" ;;
+    arm64v8 ) build_arch="arm64" ;;    
+  esac
+  case ${docker_arch} in
+    amd64   ) gosu_arch="amd64" ;;
+    arm32v6 ) gosu_arch="armhf" ;;
+    arm64v8 ) gosu_arch="arm64" ;;    
+  esac
+  cp -rf Dockerfile.cross Dockerfile.${docker_arch}
+  sed -i"" "s/__BASEIMAGE_ARCH__/${docker_arch}/g" ./Dockerfile.${docker_arch}
+  sed -i"" "s/__BUILD_ARCH__/${build_arch}/g" ./Dockerfile.${docker_arch}
+  sed -i"" "s/__GOSU_ARCH__/${gosu_arch}/g" ./Dockerfile.${docker_arch}
+done
+
+for arch in amd64 arm32v6 arm64v8; do
+  docker build -f Dockerfile.${arch} -t arrkiin/ledisdb:${arch}-latest .
+  docker push arrkiin/ledisdb:${arch}-latest
+done
+
+docker manifest create --amend ${repo}/${project}:latest ${repo}/${project}:amd64-latest ${repo}/${project}:arm32v6-latest ${repo}/${project}:arm64v8-latest
+docker manifest annotate ${repo}/${project}:latest ${repo}/${project}:arm32v6-latest --os linux --arch arm
+docker manifest annotate ${repo}/${project}:latest ${repo}/${project}:arm64v8-latest --os linux --arch arm64 --variant armv8
+docker manifest push ${repo}/${project}:latest
